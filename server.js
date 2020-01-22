1faa93f59a64cf25c952630ff902ef84e07c4e5b
EunsooJung <hi.michael.jung@gmail.com>
2020-01-22 00:58:45 -0800

    Modified local static content and added method-override dependency

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 
 // setup middleware
 const app = express();
 const PORT = process.env.PORT || 2000;
 
 // To use local static file
-app.use(express.static('public'));
+app.use(express.static(process.cwd() + '/public'));
 
 // setup body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// setup method-override
+app.use(methodOverride('_method'));
 
 // Setup Handlebars engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
