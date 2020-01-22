591403f8393e00f127cd23402b4c9e1666d23a8d
EunsooJung <hi.michael.jung@gmail.com>
2020-01-22 01:19:50 -0800

    Implemented controllers

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+const burger = require('../models/burger');
+
+router.get('/', function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObj = {
+      bugers: data
+    };
+    console.log(hbsObj);
+    res.render('index', hbsObj);
+  });
+});
+
+router.post('/', function(req, res) {
+  burger.insertOne(
+    ['burger_name', 'devoured'],
+    [req.body.burger_name, '0'],
+    function() {
+      res.redirect('/');
+    }
+  );
+});
+
+router.put(':/id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  console.log('condition', condition);
+
+  burger.updateOne({ devoured: req.body.devoured }, condition, function() {
+    res.redirect('/');
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+  burger.deleteOne(condition, function() {
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
