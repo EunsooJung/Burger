2656052950decb6890e26040c9e3f61dae312084
EunsooJung <hi.michael.jung@gmail.com>
2020-01-21 23:33:16 -0800

    Added DB Connection

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+/**
+ * Connection to Heroku or Local MySQL DB
+ */
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWDB_URL) {
+  connection = mysql.createConnection(process.env.JAWDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+  });
+}
+
+module.exports = connection;
